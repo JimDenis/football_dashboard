235c159342e366176f7d524f91d3f24d81c0d218
Jim Denis <jimdenis007@yahoo.com>
2020-10-06 14:40:51 -0700

    init commit fix 2

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
+    //    if (err) throw err;
     console.log("Connected to the MySQL database!");
 });
 
