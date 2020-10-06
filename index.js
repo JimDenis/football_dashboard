8c44585dc0713b9190ce6346ddbc84465cfc0223
Jim Denis <jimdenis007@yahoo.com>
2020-10-06 16:30:30 -0700

    fixing DB connection

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ if (process.env.JAWSDB_URL) {
 }
 
 connection.connect(function (err) {
-    if (err) throw err;
+    //if (err) throw err;
     console.log("Connected to the MySQL database!");
 });
 
