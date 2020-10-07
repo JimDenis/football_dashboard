caf37e242756651dd28bd30f61e6697dfdbeca09
Jim Denis <jimdenis007@yahoo.com>
2020-10-07 10:21:17 -0700

    Fixing user screen

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,23 @@ const router = express.Router();
 var mysql = require("mysql");
 
 // connect to MySQL DB
-var connection = mysql.createConnection({
-    host: "localhost",
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+        // Your port; if not 3306
+        port: 3306,
 
-    // Your username
-    user: "root",
+        // Your username
+        user: "root",
 
-    // Your password
-    password: "",
-    database: "footballpooldb",
-});
+        // Your password
+        password: "",
+        database: "footballpooldb",
+    });
+}
 
 router.get("/players/", (req, res) => {
     let playerId = req.session.user.username;
